0e52e46b638bbfd514bab4cb93db50dc37dc7b73
Charles Kornoelje <charleskornoelje@gmail.com>
2021-01-20 21:27:33 -0500

    reorder for linting to pass

--- packages/harmoniously/src/index.ts
+++ packages/harmoniously/src/index.ts
@@ -1,7 +1,8 @@
-export * from "./harmony";
-
 // for testing
 import { harmony } from "./harmony";
 import { rooms, times, user_constraints } from "./temp";
 
 harmony(user_constraints, undefined, [times, rooms], false);
+
+// export
+export * from "./harmony";
